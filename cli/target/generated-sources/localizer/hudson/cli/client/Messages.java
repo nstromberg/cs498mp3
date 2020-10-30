5a855552704748fa08200c451b280d41a2ab3974
Nathan Stromberg <nathan.b.stromberg@gmail.com>
2020-10-30 15:52:16 -0400

    Integrated with Eclipse

--- cli/target/generated-sources/localizer/hudson/cli/client/Messages.java
+++ cli/target/generated-sources/localizer/hudson/cli/client/Messages.java
@@ -24,6 +24,76 @@ public class Messages {
     private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
 
     /**
+     * Key {@code CLI.NoSuchFileExists}: {@code No such file exists: {0}}.
+     * 
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code No such file exists: {0}}
+     */
+    public static String CLI_NoSuchFileExists(Object arg0) {
+        return holder.format("CLI.NoSuchFileExists", arg0);
+    }
+
+    /**
+     * Key {@code CLI.NoSuchFileExists}: {@code No such file exists: {0}}.
+     * 
+     * @param arg0
+     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
+     * @return
+     *     {@code No such file exists: {0}}
+     */
+    public static Localizable _CLI_NoSuchFileExists(Object arg0) {
+        return new Localizable(holder, "CLI.NoSuchFileExists", arg0);
+    }
+
+    /**
+     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
+     * cannot work with this Jenkins server.}.
+     * 
+     * @return
+     *     {@code Version mismatch. This CLI cannot work with this Jenkins
+     *     server.}
+     */
+    public static String CLI_VersionMismatch() {
+        return holder.format("CLI.VersionMismatch");
+    }
+
+    /**
+     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
+     * cannot work with this Jenkins server.}.
+     * 
+     * @return
+     *     {@code Version mismatch. This CLI cannot work with this Jenkins
+     *     server.}
+     */
+    public static Localizable _CLI_VersionMismatch() {
+        return new Localizable(holder, "CLI.VersionMismatch");
+    }
+
+    /**
+     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
+     * is specified.}.
+     * 
+     * @return
+     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
+     */
+    public static String CLI_NoURL() {
+        return holder.format("CLI.NoURL");
+    }
+
+    /**
+     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
+     * is specified.}.
+     * 
+     * @return
+     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
+     */
+    public static Localizable _CLI_NoURL() {
+        return new Localizable(holder, "CLI.NoURL");
+    }
+
+    /**
      * Key {@code CLI.Usage}: {@code Jenkins CLI
      * Usage: java -jar
      * jenkins-cli.jar [-s URL] command [opts...] args...
@@ -117,76 +187,6 @@ public class Messages {
      */
     public static Localizable _CLI_Usage() {
         return new Localizable(holder, "CLI.Usage");
-    }
-
-    /**
-     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
-     * is specified.}.
-     * 
-     * @return
-     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
-     */
-    public static String CLI_NoURL() {
-        return holder.format("CLI.NoURL");
-    }
-
-    /**
-     * Key {@code CLI.NoURL}: {@code Neither -s nor the JENKINS_URL env var
-     * is specified.}.
-     * 
-     * @return
-     *     {@code Neither -s nor the JENKINS_URL env var is specified.}
-     */
-    public static Localizable _CLI_NoURL() {
-        return new Localizable(holder, "CLI.NoURL");
-    }
-
-    /**
-     * Key {@code CLI.NoSuchFileExists}: {@code No such file exists: {0}}.
-     * 
-     * @param arg0
-     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
-     * @return
-     *     {@code No such file exists: {0}}
-     */
-    public static String CLI_NoSuchFileExists(Object arg0) {
-        return holder.format("CLI.NoSuchFileExists", arg0);
-    }
-
-    /**
-     * Key {@code CLI.NoSuchFileExists}: {@code No such file exists: {0}}.
-     * 
-     * @param arg0
-     *      1st format parameter, {@code {0}}, as {@link String#valueOf(Object)}.
-     * @return
-     *     {@code No such file exists: {0}}
-     */
-    public static Localizable _CLI_NoSuchFileExists(Object arg0) {
-        return new Localizable(holder, "CLI.NoSuchFileExists", arg0);
-    }
-
-    /**
-     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
-     * cannot work with this Jenkins server.}.
-     * 
-     * @return
-     *     {@code Version mismatch. This CLI cannot work with this Jenkins
-     *     server.}
-     */
-    public static String CLI_VersionMismatch() {
-        return holder.format("CLI.VersionMismatch");
-    }
-
-    /**
-     * Key {@code CLI.VersionMismatch}: {@code Version mismatch. This CLI
-     * cannot work with this Jenkins server.}.
-     * 
-     * @return
-     *     {@code Version mismatch. This CLI cannot work with this Jenkins
-     *     server.}
-     */
-    public static Localizable _CLI_VersionMismatch() {
-        return new Localizable(holder, "CLI.VersionMismatch");
     }
 
 }
